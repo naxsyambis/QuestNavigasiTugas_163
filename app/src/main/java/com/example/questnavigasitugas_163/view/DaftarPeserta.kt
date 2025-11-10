97bf3f9c6d4cc7d5f5847054622e712c4a5204c3
naxsyambis <naxsyambis@gmail.com>
2025-11-10 17:08:29 +0700

    membuat fungsi DaftarPeserta.kt

--- app/src/main/java/com/example/questnavigasitugas_163/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/view/DaftarPeserta.kt
@@ -3,3 +3,9 @@ package com.example.questnavigasitugas_163.view
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DaftarPeserta(
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit
+) {}
