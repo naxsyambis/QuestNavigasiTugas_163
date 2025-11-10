147ed0a7945054f68b22a0f89e965dfc09d6569b
naxsyambis <naxsyambis@gmail.com>
2025-11-10 15:10:03 +0700

    membuat column untuk mengatur tampilan di beranda dan menambah values gambar

--- app/src/main/java/com/example/questnavigasitugas_163/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/view/Beranda.kt
@@ -1,10 +1,31 @@
 package com.example.questnavigasitugas_163.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasitugas_163.R
 
 @Composable
 fun Beranda(
     onMasukClick: () -> Unit
 ) {
-
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {}
+    }
 }
