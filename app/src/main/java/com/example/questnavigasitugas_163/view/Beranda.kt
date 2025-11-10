5825502e23697b5869a0a1ab4025ad8d986446a4
naxsyambis <naxsyambis@gmail.com>
2025-11-10 16:26:14 +0700

    memperbaiki eror

--- app/src/main/java/com/example/questnavigasitugas_163/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/view/Beranda.kt
@@ -1,14 +1,19 @@
 package com.example.questnavigasitugas_163.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.questnavigasitugas_163.R
 
@@ -26,6 +31,31 @@ fun Beranda(
                 .padding(32.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {}
+        ) {
+            // Gambar Logo
+            Image(
+                painter = painterResource(id = R.drawable.logo_aplikasi),
+                contentDescription = stringResource(R.string.deskripsi_logo),
+                modifier = Modifier.size(120.dp)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Teks Selamat Datang
+            Text(
+                text = stringResource(R.string.selamat_datang),
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
+
+
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BerandaPreview() {
+    Beranda(onMasukClick = {})
 }
