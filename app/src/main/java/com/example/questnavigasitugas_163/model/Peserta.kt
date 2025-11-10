354cc9a22e4e7a9610d5386d1cab0df911690681
naxsyambis <naxsyambis@gmail.com>
2025-11-10 16:30:16 +0700

    membuat fungsi DafatarPeserta

--- app/src/main/java/com/example/questnavigasitugas_163/model/Peserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/model/Peserta.kt
@@ -1,2 +1,13 @@
 package com.example.questnavigasitugas_163.model
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DaftarPeserta(
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit
+) {
+
+}
