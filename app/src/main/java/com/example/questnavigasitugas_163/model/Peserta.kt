76c7f6c361d7bb80151c279ad23203c069baa566
naxsyambis <naxsyambis@gmail.com>
2025-11-10 16:33:58 +0700

    ada kesalahan, membuat file berisi data list

--- app/src/main/java/com/example/questnavigasitugas_163/model/Peserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/model/Peserta.kt
@@ -1,13 +1,2 @@
 package com.example.questnavigasitugas_163.model
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DaftarPeserta(
-    onBerandaClick: () -> Unit,
-    onFormulirClick: () -> Unit
-) {
-
-}
