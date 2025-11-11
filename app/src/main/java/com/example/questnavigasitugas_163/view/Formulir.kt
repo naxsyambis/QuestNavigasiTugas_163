0608361938e5f0808c503f85e28004a0ac50e653
naxsyambis <naxsyambis@gmail.com>
2025-11-11 11:05:32 +0700

    membuat fungsi formulir

--- app/src/main/java/com/example/questnavigasitugas_163/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/view/Formulir.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigasitugas_163.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun Formulir(
+    onKembaliClick: () -> Unit
+) {}
