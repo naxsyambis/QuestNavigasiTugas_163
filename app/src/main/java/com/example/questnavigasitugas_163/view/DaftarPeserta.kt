48e088ebc2246fe78b6692cf21b1acab1438c275
naxsyambis <naxsyambis@gmail.com>
2025-11-11 10:51:22 +0700

    membuat lazy column yang akan berisi data yang sudah kita buat secara internal

--- app/src/main/java/com/example/questnavigasitugas_163/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_163/view/DaftarPeserta.kt
@@ -3,7 +3,11 @@ package com.example.questnavigasitugas_163.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -17,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.questnavigasitugas_163.R
+import com.example.questnavigasitugas_163.model.DataPeserta
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,5 +63,17 @@ fun DaftarPeserta(
                 }
             }
         }
-    ) {}
+    ) {paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(DataPeserta.daftarPeserta) { peserta ->
+                KartuPeserta(peserta = peserta)
+            }
+        }
+    }
 }
